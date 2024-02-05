e9922538a47c1cb38e1d1b22fad4ca71e28a920d
Davit Badalyan <davit.badalyan.00@gmail.com>
2024-02-05 23:45:10 +0400

    SceneInstaller initial setup

---
+++ Assets/__Project__/Scripts/Core/Contexts/Installers/SceneInstaller.cs
@@ -0,0 +1,28 @@
+using Zenject;
+
+namespace Core.Contexts
+{
+    public class SceneInstaller : MonoInstaller
+    {
+        #region SERIALIZED_VARIABLES
+
+        //
+
+        #endregion
+
+        #region OVERRIDDEN_FUNCTIONS
+
+        public override void InstallBindings()
+        {
+            //
+        }
+
+        #endregion
+
+        #region PRIVATE_FUNCTIONS
+
+        //
+
+        #endregion
+    }
+}
